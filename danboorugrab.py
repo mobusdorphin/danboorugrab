af242fe05e621d76a8ec2828fe12534496340121
Mobus Dorphin <celestialtuba@gmail.com>
2015-09-30 12:41:26 -0400

    Added comments

--- danboorugrab.py
+++ danboorugrab.py
@@ -6,11 +6,19 @@ import wget
 import sys
 import os
 
+
+###########################
+# Set some variables
+
 outputString = ''
 urlList = []
 notFound = True
 tagString = ''
 urlString = ''
+
+###########################
+# Determine tags to search
+
 try: 
   sys.argv[1]
   for i in sys.argv[1:]:
@@ -19,6 +27,10 @@ try:
 except IndexError:
   tagString = raw_input('Enter tags separated by spaces or by +:  ').replace(' ', '+')
 sourceUrl = 'http://danbooru.donmai.us/posts?tags=' + tagString
+
+###########################
+# Keep checking pages as long as we're getting a result and tabulate each URL to a list
+
 while notFound:
   print "Checking page %s" % sourceUrl
   r = requests.get(sourceUrl)
@@ -34,23 +46,33 @@ while notFound:
     notFound = False
 for i in urlList:
   urlString += i + '\n'
+
+###########################
+# Confirm with user everything is kosher before writing to disk
+
 print 'Found %s items.  List URLs? y/N' % len(urlList)
 toList = raw_input()
 toList = toList.lower()
 if toList == 'y' or toList == 'yes':
   print urlString
+
 print 'Save URL list? y/N'
 saveList = raw_input()
 if saveList == 'y' or saveList == 'yes':
   f = open(tagString + '.txt', 'w')
   f.write(urlString)
   f.close()
+
 print 'Download them all? y/N'
 downloadThem = raw_input()
+
+###########################
+# Have user specify a directory and start downloading.
+
 if downloadThem == 'y' or downloadThem == 'yes':
   failed = True
   while failed:
-    directory = raw_input('Save in what directory?:  ')
+    directory = raw_input('Save in what directory?:  (Leave blank for current dir)')
     try:
       if directory == '':
         break
