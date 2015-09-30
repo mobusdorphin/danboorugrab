64f238ba631f4c3604655955238936d0b5d4fb8b
Mobus Dorphin <celestialtuba@gmail.com>
2015-09-30 10:11:32 -0400

    Accepts tags as user input in addition to command line args

--- danboorugrab.py
+++ danboorugrab.py
@@ -10,10 +10,14 @@ urlList = []
 notFound = True
 tagString = ''
 urlString = ''
-for i in sys.argv[1:]:
-  tagString += i + '+'
-tagString = tagString[:-1]
-sourceUrl = 'http://danbooru.donmai.us/posts?&tags=' + tagString
+try: 
+  sys.argv[1]
+  for i in sys.argv[1:]:
+    tagString += i + '+'
+  tagString = tagString[:-1]
+except IndexError:
+  tagString = raw_input('Enter tags separated by spaces or by +:  ').replace(' ', '+')
+sourceUrl = 'http://danbooru.donmai.us/posts?tags=' + tagString
 while notFound:
   print "Checking page %s" % sourceUrl
   r = requests.get(sourceUrl)
@@ -27,19 +31,19 @@ while notFound:
     sourceUrl = 'http://danbooru.donmai.us' + re.search('next" href="(.+?)"', r.text).group(1)
   except AttributeError:
     notFound = False
+for i in urlList:
+  urlString += i + '\n'
 print 'Found %s items.  List URLs? y/N' % len(urlList)
 toList = raw_input()
 toList = toList.lower()
 if toList == 'y' or toList == 'yes':
-  for i in urlList:
-    urlString += i + '\n'
   print urlString
-  print 'Save URL list? y/N'
-  saveList = raw_input()
-  if saveList == 'y'or savelist == 'yes':
-    f = open(tagString + '.txt', 'w')
-    f.write(urlString)
-    f.close()
+print 'Save URL list? y/N'
+saveList = raw_input()
+if saveList == 'y' or saveList == 'yes':
+  f = open(tagString + '.txt', 'w')
+  f.write(urlString)
+  f.close()
 print 'Download them all? y/N'
 downloadThem = raw_input()
 if downloadThem == 'y' or downloadThem == 'yes':
@@ -48,3 +52,5 @@ if downloadThem == 'y' or downloadThem == 'yes':
     print '\nDownloading file %s of %s' % (dlcount, len(urlList))
     wget.download(i)
     dlcount += 1
+
+print '\n'
