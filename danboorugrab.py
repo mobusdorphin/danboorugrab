71402de12ece5d18b22da7cbbddd28a6e6c0bde3
Mobus Dorphin <celestialtuba@gmail.com>
2015-09-30 18:15:14 -0400

    Allows file to be created if does not exist.  Therefore also added additional security checks because of that

--- danboorugrab.py
+++ danboorugrab.py
@@ -58,6 +58,7 @@ if toList == 'y' or toList == 'yes':
 
 print 'Save URL list? y/N'
 saveList = raw_input()
+saveList = saveList.lower()
 if saveList == 'y' or saveList == 'yes':
   f = open(tagString + '.txt', 'w')
   f.write(urlString)
@@ -65,21 +66,52 @@ if saveList == 'y' or saveList == 'yes':
 
 print 'Download them all? y/N'
 downloadThem = raw_input()
+downloadThem = downloadThem.lower()
 
 ###########################
-# Have user specify a directory and start downloading.
+# Have user specify a target directory 
 
 if downloadThem == 'y' or downloadThem == 'yes':
-  failed = True
-  while failed:
+  dirNotConfirmed = True
+  while dirNotConfirmed:
     directory = raw_input('Save in what directory?:  (Leave blank for current dir)')
     try:
-      if directory == '':
+      if directory == '': # If blank, use current directory
         break
-      os.chdir(directory)
-      failed = False
+      if os.path.isdir(directory): # If directory exists, change to it
+        os.chdir(directory)
+      elif os.path.isfile(directory): # If exists but is a file, complain and try again
+        print 'File exists but is not a directory.  Please enter a valid directory\n'
+        continue
+      else:
+        print '%s does not exist.  Create?:  y/N' % directory # Otherwise, offer to create the directory
+        createDirectory = raw_input()
+        createDirectory = createDirectory.lower()  
+        if createDirectory == 'y' or createDirectory == 'yes':
+          if os.path.exists(directory):
+#                 If for some reason the path is created between the time we checked if the file 
+#                 exists and when we want to create it, complain and try again
+            print 'Something went terribly wrong.  Please specify another directory' 
+            continue
+          os.makedirs(directory)
+          os.chdir(directory)
+        else:
+          continue
+      print 'Preparing to download %s files to %s.  Confirm y/N' % (len(urlList), os.getcwd())
+      confirmation = raw_input() # Finally, confirm that everything we've done is fully kosher for passover
+      confirmation = confirmation.lower()
+      if confirmation == 'y' or confirmation == 'yes': # If so, allow dirNotConfirmed to change value, which  
+#                                               will break the loop allowing us all to move on with our lives
+        pass
+      else:            # Otherwise, we do it all over again
+        continue
+      dirNotConfirmed = False
     except OSError as oops:
       print oops
+
+###########################
+# Finally, download the things
+
   dlcount = 1
   for i in urlList:
     filename = re.search('data/(.+)', i).group(1)
@@ -92,3 +124,4 @@ if downloadThem == 'y' or downloadThem == 'yes':
     dlcount += 1
 
 print '\n'
+exit(0)
