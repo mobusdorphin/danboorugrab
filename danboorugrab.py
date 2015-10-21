9874c47a2264e56970cb910f9f0187ca2d37077d
Mobus Dorphin <celestialtuba@gmail.com>
2015-10-21 12:55:54 -0400

    Functionalized the script

--- danboorugrab.py
+++ danboorugrab.py
@@ -1,89 +1,17 @@
 #!/usr/bin/python
 
-import requests
-import re
-import wget
-import sys
 import os
+import re
+import requests
+import sys
+import wget
+import Tkinter
 
 
-###########################
-# Set some variables
-
-outputString = ''
-urlList = []
-notFound = True
-tagString = ''
-urlString = ''
-
-###########################
-# Determine tags to search
-
-try: 
-  sys.argv[1]
-  for i in sys.argv[1:]:
-    tagString += i + '+'
-  tagString = tagString[:-1]
-except IndexError:
-  tagString = raw_input('Enter tags separated by spaces or by +:  ').replace(' ', '+')
-sourceUrl = 'http://danbooru.donmai.us/posts?tags=' + tagString
-
-###########################
-# Keep checking pages as long as we're getting a result and tabulate each URL to a list
-
-while notFound:
-  print "Checking page %s" % sourceUrl
-  try: r = requests.get(sourceUrl)
-  except: 
-    print 'Request failed, retrying...'
-    try: r = requests.get(sourceUrl)
-    except: 
-      print 'Request failed, retrying...'
-      try: r = requests.get(sourceUrl)
-      except: 
-        print 'Request failed after three attempts.  Aborting'
-        exit(1)
-  match = re.findall('data-file-url="(.+?)"', r.text)
-  if len(match) == 0:
-    notFound = False
-  for i in match:
-    url = 'http://danbooru.donmai.us' + i
-    urlList.append(url)
-  try:
-    sourceUrl = 'http://danbooru.donmai.us' + re.search('next" href="(.+?)"', r.text).group(1)
-  except AttributeError:
-    notFound = False
-for i in urlList:
-  urlString += i + '\n'
-
-###########################
-# Confirm with user everything is kosher before writing to disk
-
-print 'Found %s items.  List URLs? y/N' % len(urlList)
-toList = raw_input()
-toList = toList.lower()
-if toList == 'y' or toList == 'yes':
-  print urlString
-
-print 'Save URL list? y/N'
-saveList = raw_input()
-saveList = saveList.lower()
-if saveList == 'y' or saveList == 'yes':
-  f = open(tagString + '.txt', 'w')
-  f.write(urlString)
-  f.close()
-
-print 'Download them all? y/N'
-downloadThem = raw_input()
-downloadThem = downloadThem.lower()
-
-###########################
-# Have user specify a target directory 
-
-if downloadThem == 'y' or downloadThem == 'yes':
+def downloadTheThings(urlList):
   dirNotConfirmed = True
   while dirNotConfirmed:
-    directory = raw_input('Save in what directory?:  (Leave blank for current dir)')
+    directory = raw_input('Save in what directory?  (Leave blank for current dir):  ')
     try:
       if directory == '': # If blank, use current directory
         break
@@ -98,8 +26,8 @@ if downloadThem == 'y' or downloadThem == 'yes':
         createDirectory = createDirectory.lower()  
         if createDirectory == 'y' or createDirectory == 'yes':
           if os.path.exists(directory):
-#                 If for some reason the path is created between the time we checked if the file 
-#                 exists and when we want to create it, complain and try again
+  #                 If for some reason the path is created between the time we checked if the file 
+  #                 exists and when we want to create it, complain and try again
             print 'Something went terribly wrong.  Please specify another directory' 
             continue
           os.makedirs(directory)
@@ -110,7 +38,7 @@ if downloadThem == 'y' or downloadThem == 'yes':
       confirmation = raw_input() # Finally, confirm that everything we've done is fully kosher for passover
       confirmation = confirmation.lower()
       if confirmation == 'y' or confirmation == 'yes': # If so, allow dirNotConfirmed to change value, which  
-#                                               will break the loop allowing us all to move on with our lives
+  #                                               will break the loop allowing us all to move on with our lives
         pass
       else:            # Otherwise, we do it all over again
         continue
@@ -118,8 +46,8 @@ if downloadThem == 'y' or downloadThem == 'yes':
     except OSError as oops:
       print oops
 
-###########################
-# Finally, download the things
+  ###########################
+  # Finally, download the things
 
   dlcount = 1
   for i in urlList:
@@ -131,6 +59,88 @@ if downloadThem == 'y' or downloadThem == 'yes':
       continue
     wget.download(i)
     dlcount += 1
+
+
+
+
+def getTags():
+  tagString = ''
+  try: 
+    sys.argv[1]
+    for i in sys.argv[1:]:
+      tagString += i + '+'
+    tagString = tagString[:-1]
+  except IndexError:
+    tagString = raw_input('Enter tags separated by spaces or by +:  ').replace(' ', '+')
+  sourceUrl = 'http://danbooru.donmai.us/posts?tags=' + tagString
+  return tagString, sourceUrl
+
+###########################
+# Keep checking pages as long as we're getting a result and tabulate each URL to a list
+def obtainUrls(tagString, sourceUrl):
+  urlList = []
+  urlString = ''
+  notFound = True
+  while notFound:
+    print "Checking page %s" % sourceUrl
+    try: r = requests.get(sourceUrl)
+    except: 
+      print 'Request failed, retrying...'
+      try: r = requests.get(sourceUrl)
+      except: 
+	print 'Request failed, retrying...'
+	try: r = requests.get(sourceUrl)
+	except: 
+	  print 'Request failed after three attempts.  Aborting'
+	  exit(1)
+    match = re.findall('data-file-url="(.+?)"', r.text)
+    if len(match) == 0:
+      notFound = False
+    for i in match:
+      url = u'http://danbooru.donmai.us' + i
+      urlList.append(url)
+    try:
+      sourceUrl = 'http://danbooru.donmai.us' + re.search('next" href="(.+?)"', r.text).group(1)
+    except AttributeError:
+      notFound = False
+  for i in urlList:
+    urlString += i + '\n'
+  print 'Found %s items.  List URLs? y/N' % len(urlList)
+  toList = lowerInput()
+  if toList == 'y' or toList == 'yes':
+    print urlString
+
+  print 'Save URL list? y/N'
+  saveList = lowerInput() 
+  if saveList == 'y' or saveList == 'yes':
+    f = open(tagString + '.txt', 'w')
+    f.write(urlString)
+    f.close()
+    return urlList, urlString
+
+def lowerInput():
+  s = raw_input()
+  return s.lower()
+
+
+
+urlList = []
+urlString = ''
+tagString = ''
+tagString, sourceUrl = getTags()
+urlList, urlString = obtainUrls(tagString, sourceUrl)
+
+###########################
+# Confirm with user everything is kosher before writing to disk
+
+print 'Download them all? y/N'
+downloadThem = lowerInput() 
+
+###########################
+# Have user specify a target directory 
+
+if downloadThem == 'y' or downloadThem == 'yes':
+  downloadTheThings(urlList)
 
 print '\n'
 exit(0)
